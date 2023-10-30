acc54d0fad2853154ab8f9529524ecf466c106d8
Devadathan M B <devadathanmb@gmail.com>
2023-10-30 17:23:22 +0530

    change firebase env to base64

--- src/db/initDb.ts
+++ src/db/initDb.ts
@@ -1,7 +1,13 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
+// I wasted an entire day trying to pass a json to docker run env
+// life is too short for ci
+const base64ServiceAccount = process.env.FIREBASE_SERVICE_ACCOUNT!;
+const serviceAccountBuffer = Buffer.from(base64ServiceAccount, "base64");
+const serviceAccountJson = serviceAccountBuffer.toString("utf-8");
+
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 function initDb() {
   initializeApp({
