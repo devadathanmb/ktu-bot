00ea5729987957e6463e496aac2ed26ab96335e3
Devadathan M B <devadathanmb@gmail.com>
2023-11-28 12:43:12 +0530

    Attach academic command handlers to app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import start from "./handlers/start";
 import help from "./handlers/help";
 import cancel from "./handlers/cancel";
 import result from "./handlers/result";
+import calendar from "./handlers/calendar";
 import search from "./handlers/search";
 import code from "./handlers/code";
 import notifications from "./handlers/notifications";
@@ -12,6 +13,7 @@ import unsubscribe from "./handlers/unsubscribe";
 import defaultHandler from "./handlers/defaultHandler";
 import { Scenes, session } from "telegraf";
 import resultWizard from "./scenes/resultWizard";
+import academicCalendarWizard from "./scenes/calendarWizard";
 import announcementWizard from "./scenes/announcementWizard";
 import { CustomContext } from "./types/customContext.type";
 import availableCommands from "./constants/availableCommands";
@@ -29,6 +31,7 @@ const bot = new Telegraf<CustomContext>(process.env.BOT_TOKEN!);
 const stage = new Scenes.Stage<CustomContext>([
   resultWizard,
   announcementWizard,
+  academicCalendarWizard,
 ]);
 
 bot.use(loggingMiddleware);
@@ -46,12 +49,15 @@ bot.command("cancel", async (ctx) => await cancel(ctx));
 bot.command("notifications", async (ctx) => await notifications(ctx));
 bot.command("subscribe", async (ctx) => await subscribe(ctx, db));
 bot.command("unsubscribe", async (ctx) => await unsubscribe(ctx, db));
+bot.command("calendar", async (ctx) => await calendar(ctx));
 bot.on("inline_query", async (ctx) => await searchInlineQueryHandler(ctx));
 bot.on("chosen_inline_result", async (ctx) => {
   await inlineQueryResultHandler(ctx.chosenInlineResult, bot);
 });
 bot.on("message", async (ctx) => await defaultHandler(ctx));
 
+// The top level error handler
+// this will catch any errors that may happen
 bot.catch((error) => {
   console.log("telegraf error", error);
 });
