328f314e2d9ef3c5d765f66cd34056a7210176d8
Devadathan M B <devadathanmb@gmail.com>
2023-11-28 12:29:30 +0530

    fix: Modify cancel command response to include calendar command

--- src/handlers/cancel.ts
+++ src/handlers/cancel.ts
@@ -2,7 +2,14 @@ import { CustomContext } from "../types/customContext.type";
 
 async function cancel(ctx: CustomContext) {
   await ctx.reply(
-    "There are no on going operations to cancel.\n\nUse /result to start a result lookup or /notifications to start an notifications lookup.",
+    `There are no on going operations to cancel.
+
+Please use:
+
+/result : To start a result look up
+/notifications : To start a notification look up
+/calendar : To start an academic calendar look up
+`,
   );
 }
 
