aa81c5f6985e4a2b54d7b5f1793c9061f125ce11
Devadathan M B <devadathanmb@gmail.com>
2023-11-28 12:29:15 +0530

    feat: Add calendar command handler

---
+++ src/handlers/calendar.ts
@@ -0,0 +1,7 @@
+import { CustomContext } from "../types/customContext.type";
+
+async function calendar(ctx: CustomContext) {
+  await ctx.scene.enter("academic-calendar-wizard");
+}
+
+export default calendar;
