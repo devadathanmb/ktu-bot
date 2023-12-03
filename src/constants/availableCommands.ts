9dc1db380c61885677c99d8fd414d76bf714f020
Devadathan M B <devadathanmb@gmail.com>
2023-12-03 12:37:18 +0530

    add timetable commands to available commands

--- src/constants/availableCommands.ts
+++ src/constants/availableCommands.ts
@@ -8,6 +8,7 @@ const availableCommands: Command[] = [
   { command: "result", description: "Fetch your exam results" },
   { command: "notifications", description: "Fetch latest KTU notifications" },
   { command: "calendar", description: "Fetch KTU academic calendars" },
+  { command: "timetable", description: "Fetch KTU exam time tables" },
   { command: "search", description: "See how to search for KTU notifications" },
   { command: "cancel", description: "Cancel any ongoing operation" },
   { command: "subscribe", description: "Subscribe to notifications" },
