cc2704483fe1691245185ee63638cd0074383927
Devadathan M B <devadathanmb@gmail.com>
2023-12-05 00:42:19 +0530

    modify code handler response

--- src/handlers/code.ts
+++ src/handlers/code.ts
@@ -6,6 +6,8 @@ This bot is fully open source! 🌐
 
 Check out the code in the <a href="https://github.com/devadathanmb/ktu-bot.git">GitHub repo</a>.
 
+Liked the bot? Consider giving it a ⭐️ on GitHub.
+
 Feel free to contribute to the project, or report any issues you encounter.
 `;
   await ctx.replyWithHTML(replyMsg);
