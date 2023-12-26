2d8e73ad16c4c74cdb6a524e015b4745753719ed
Devadathan M B <devadathanmb@gmail.com>
2023-12-26 09:48:38 +0530

    fix: Add missing command in cancel response

--- src/handlers/cancel.ts
+++ src/handlers/cancel.ts
@@ -9,6 +9,7 @@ Please use:
 /result : To start a result look up
 /notifications : To start a notification look up
 /calendar : To start an academic calendar look up
+/timetable : To start a timetable look up
 `,
   );
 }
