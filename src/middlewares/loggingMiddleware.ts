4fb2ef8309cb4e097823c9ca089fba79ae208e39
Devadathan M B <devadathanmb@gmail.com>
2023-12-31 11:52:31 +0530

    fix: Handle undefined in logging middleware [skip ci]

--- src/middlewares/loggingMiddleware.ts
+++ src/middlewares/loggingMiddleware.ts
@@ -1,14 +1,14 @@
 import { Middleware } from "telegraf";
 import { CustomContext } from "../types/customContext.type";
 
+// Logging middleware
 // For testing purposes
 const loggingMiddleware: Middleware<CustomContext> = async (ctx, next) => {
   const time = new Date().toString();
   console.log(
-    `Message from name ${ctx.from?.first_name} ${ctx.from?.last_name}`,
-  );
-  console.log(
-    `➡️ [${time}] ${ctx.updateType} from ${ctx.from?.first_name} ${ctx.from?.last_name} ID : ${ctx.from?.id}`,
+    `➡️ [${time}] ${ctx.updateType} from ${
+      ctx.from?.first_name ? ctx.from.first_name : ""
+    } ${ctx.from?.last_name ? ctx.from.last_name : ""}  ID : ${ctx.from?.id}`
   );
   console.log(ctx.message);
   return next();
