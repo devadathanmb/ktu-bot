6fd4d1e58226a498702a8ea5c7b66297105faaae
Devadathan M B <devadathanmb@gmail.com>
2024-01-13 22:07:51 +0530

    feat: Add basic regex filter matching function

---
+++ src/utils/findFilters.ts
@@ -0,0 +1,31 @@
+import { AVAILABLE_FILTERS } from "../constants/constants";
+// This function will find the list of course filters based on the input text message
+// using basic string matching
+
+function findFilters(msg: string): Array<string> {
+  // If the input message is empty, return the general filter
+  if (!msg) {
+    return ["general"];
+  }
+
+  // Convert the input message to lowercase
+  const lowerCaseMsg = msg.toLowerCase();
+
+  // Match the input message with the available filters
+  // Return the list of matched filters
+  let matchedFilters: Array<string> = [];
+
+  Object.keys(AVAILABLE_FILTERS).forEach((filter) => {
+    if (lowerCaseMsg.search(new RegExp(filter)) !== -1) {
+      matchedFilters.push(AVAILABLE_FILTERS[filter]);
+    }
+  });
+
+  if (matchedFilters.length === 0) {
+    matchedFilters.push("general");
+  }
+
+  return matchedFilters;
+}
+
+export default findFilters;
