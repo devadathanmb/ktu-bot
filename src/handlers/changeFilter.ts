65e0ceaff27669e16feb712473921990b849c5c5
Devadathan M B <devadathanmb@gmail.com>
2024-01-14 11:44:50 +0530

    feat: Add change filter handler

---
+++ src/handlers/changeFilter.ts
@@ -0,0 +1,45 @@
+// Handler to change notification filter of user
+import { CustomContext } from "../types/customContext.type";
+import { Firestore } from "firebase-admin/firestore";
+import { FILTERS } from "../constants/constants";
+import { Markup } from "telegraf";
+import deleteMessage from "../utils/deleteMessage";
+
+async function changeFilter(ctx: CustomContext, db: Firestore) {
+  const waitingMsg = await ctx.reply("Please wait...");
+  const chatId = ctx.chat!.id;
+  try {
+    const usersRef = db.collection("subscribedUsers").doc(chatId.toString());
+    const doc = await usersRef.get();
+    if (!doc.exists) {
+      await ctx.reply(
+        "You are not subscribed to notifications.\n\nPlease subscribe using /subscribe command to subscribe to notifications."
+      );
+      await deleteMessage(ctx, waitingMsg.message_id);
+    } else {
+      const currentFilter = FILTERS[doc.get("courseFilter")];
+      const msg = `
+Your current notification filter is set to <b>${currentFilter}</b>
+
+Choose a filter from the options below:
+`;
+      const filterButtons = Object.keys(FILTERS).map((filter) => ({
+        text: FILTERS[filter],
+        callback_data: `filter_${filter}`,
+      }));
+
+      filterButtons.push({
+        text: "Cancel operation ❌",
+        callback_data: "filter_cancel",
+      });
+
+      const keyboard = Markup.inlineKeyboard(filterButtons, { columns: 2 });
+      await ctx.replyWithHTML(msg, keyboard);
+      await deleteMessage(ctx, waitingMsg.message_id);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export default changeFilter;
