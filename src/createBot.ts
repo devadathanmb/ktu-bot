1937dff7c23c31e74b9cff64832d9b4ac470a8a0
Devadathan M B <devadathanmb@gmail.com>
2024-01-25 11:00:40 +0530

    feat: Add telegraf handler timeout

--- src/createBot.ts
+++ src/createBot.ts
@@ -35,7 +35,13 @@ function createBot() {
   const db = initDb();
 
   // Create the bot
-  const bot = new Telegraf<CustomContext>(process.env.BOT_TOKEN!);
+  const opts = {
+    // During result publish times, KTU servers will be slow to respond, this makes the API requests to be slower
+    // Axios timeout is set to 25 seconds, but telegraf will timeout before that
+    // This creates request duplication. Hence we need to increase the telegraf timeout
+    handlerTimeout: 1000 * 30,
+  };
+  const bot = new Telegraf<CustomContext>(process.env.BOT_TOKEN!, opts);
 
   // Create wizards
   const stage = new Scenes.Stage<CustomContext>([
