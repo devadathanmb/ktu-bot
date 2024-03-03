c56845d3536c273bfc86398133367ddf30b1ed07
Devadathan M B <devadathanmb@gmail.com>
2024-03-03 21:02:47 +0530

    feat: add back button during each step in result lookup

--- src/wizards/resultWizard.ts
+++ src/wizards/resultWizard.ts
@@ -1,5 +1,6 @@
 import { Markup, Scenes } from "telegraf";
 import { CustomContext } from "@/types/customContext.type";
+import { callbackQuery, message } from "telegraf/filters";
 import fetchCourses from "@/services/fetchCourses";
 import fetchPublishedResults from "@/services/fetchPublishedResults";
 import { fetchResult } from "@/services/fetchResult";
@@ -10,10 +11,41 @@ import calculateSgpa from "@/utils/calculateSgpa";
 import deleteMessage from "@/utils/deleteMessage";
 import handleError from "@/utils/handleError";
 
+/*
+  - Result lookup is designed as a WizardScene. This is similar to how an installation wizards works.
+  - Only /cancel command is defined to work inside the wizard. No other commands will work inside the wizard.
+  - Each wizard step shows a result window, which collects the required data for final result lookup.
+  - The progress from one wizard step to another is based on user response. Each user update, takes the wizard one step forward.
+  - So checks of user update should be done in the next step, not in the current step.
+  - After each step, the sent messages are deleted to keep the chat clean and avoid confusion.
+
+  - In case, the user does not perform the expected action (like not clicking button instead sending text), 
+  the wizard will be stuck in the same step and will keep asking for the expected action.
+
+  - /cancel command will delete all the messages sent by the wizard and will leave the wizard.
+
+*/
+
+/*
+  - The below wizard has 5 steps. Starting from 0 to 4.
+
+  - Step 0 -> Step 1 -> Step 2 -> Step 3 -> Step 4
+
+  - Step 0 : Fetch available courses and show them as buttons 
+  - Step 1 : Handle user's course selection and fetch available results and show them as buttons
+  - Step 2 : Handle user's result selection and ask for registration number
+  - Step 3 : Handle user's registration number response and ask for date of birth
+  - Step 4 : Handle user's date of birth response and fetch the result and show it to the user. And leave the wizard.
+
+  - Step 1, 2, 3 and 4 have a back button to go back to the previous step, which is handled in the corresponding steps.
+*/
+
 const handleCancelCommand = async (ctx: CustomContext) => {
   await deleteMessage(ctx, ctx.scene.session.waitingMsgId);
   await deleteMessage(ctx, ctx.scene.session.courseMsgId);
   await deleteMessage(ctx, ctx.scene.session.resultMsgId);
+  await deleteMessage(ctx, ctx.scene.session.regMsgId);
+  await deleteMessage(ctx, ctx.scene.session.dobMsgId);
   await ctx.reply(
     "Result look up cancelled.\n\nPlease use /result to start again."
   );
@@ -22,6 +54,8 @@ const handleCancelCommand = async (ctx: CustomContext) => {
 
 const resultWizard = new Scenes.WizardScene<CustomContext>(
   "result-wizard",
+
+  // Wizard Step 0
   async (ctx: CustomContext) => {
     try {
       const waitingMsg = await ctx.reply(
@@ -41,6 +75,8 @@ const resultWizard = new Scenes.WizardScene<CustomContext>(
       return await handleError(ctx, error);
     }
   },
+
+  // Wizard Step 1
   async (ctx) => {
     if (ctx.message) {
       return await ctx.reply(
@@ -48,10 +84,18 @@ const resultWizard = new Scenes.WizardScene<CustomContext>(
       );
     }
     await deleteMessage(ctx, ctx.scene.session.courseMsgId);
-    const courseId: number = Number.parseInt(
-      (ctx.callbackQuery as any)?.data?.split("_")[1]
-    );
-    ctx.scene.session.courseId = courseId;
+    let courseId: number;
+    if (ctx.has(callbackQuery("data"))) {
+      if (ctx.callbackQuery.data === "back_to_1") {
+        courseId = ctx.scene.session.courseId;
+      } else {
+        courseId = Number.parseInt(ctx.callbackQuery.data.split("_")[1]);
+        ctx.scene.session.courseId = courseId;
+      }
+    } else {
+      await ctx.reply("An error occured. Please try again.");
+      return ctx.scene.leave();
+    }
     try {
       const waitingMsg = await ctx.reply(
         "Fetching available results.. Please wait.."
@@ -63,7 +107,12 @@ const resultWizard = new Scenes.WizardScene<CustomContext>(
         ({ resultName, examDefId, schemeId }) =>
           Markup.button.callback(resultName, `${examDefId}_${schemeId}`)
       );
-      const keyboard = Markup.inlineKeyboard(resultButtons, { columns: 1 });
+
+      const goBackButton = Markup.button.callback("🔙 Back", "back_to_0");
+
+      const keyboard = Markup.inlineKeyboard([...resultButtons, goBackButton], {
+        columns: 1,
+      });
       const msg = await ctx.sendMessage("Choose a result:", keyboard);
       ctx.scene.session.resultMsgId = msg.message_id;
       return ctx.wizard.next();
@@ -71,30 +120,105 @@ const resultWizard = new Scenes.WizardScene<CustomContext>(
       return await handleError(ctx, error);
     }
   },
+
+  // Wizard Step 2
   async (ctx) => {
     if (ctx.message) {
       return await ctx.reply("Please choose a valid option");
     }
-    const [examDefId, schemeId] = (ctx.callbackQuery as any)?.data?.split("_");
+
     await deleteMessage(ctx, ctx.scene.session.resultMsgId);
-    ctx.scene.session.examDefId = examDefId;
-    ctx.scene.session.schemeId = schemeId;
-    await ctx.reply("Please enter your KTU Registration Number");
+    if (
+      ctx.has(callbackQuery("data")) &&
+      ctx.callbackQuery.data === "back_to_0"
+    ) {
+      ctx.wizard.selectStep(0);
+
+      if (typeof ctx.wizard.step === "function") {
+        return ctx.wizard.step(ctx, async () => {});
+      }
+    }
+    if (!ctx.has(callbackQuery("data"))) {
+      ctx.reply("An error occured. Please try again.");
+      return ctx.scene.leave();
+    }
+    const [examDefId, schemeId] = ctx.callbackQuery.data.split("_");
+    ctx.scene.session.examDefId = Number(examDefId);
+    ctx.scene.session.schemeId = Number(schemeId);
+    const msg = await ctx.reply("Please enter your KTU Registration Number", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "🔙 Back",
+              callback_data: "back_to_1",
+            },
+          ],
+        ],
+      },
+    });
+    ctx.scene.session.regMsgId = msg.message_id;
     return ctx.wizard.next();
   },
+
+  // Wizard Step 3
   async (ctx) => {
-    const regisNo: string = (ctx.message as any)?.text;
+    await deleteMessage(ctx, ctx.scene.session.regMsgId);
+    if (
+      ctx.has(callbackQuery("data")) &&
+      ctx.callbackQuery.data === "back_to_1"
+    ) {
+      ctx.wizard.selectStep(1);
+      if (typeof ctx.wizard.step === "function") {
+        return ctx.wizard.step(ctx, async () => {});
+      }
+    }
+
+    if (!ctx.has(message("text"))) {
+      return await ctx.reply("Please enter a valid registration number");
+    }
+    const regisNo: string = ctx.message.text;
     if (!regisNo) {
       return await ctx.reply("Please enter a valid registration number");
     }
     ctx.scene.session.regisNo = regisNo.toUpperCase();
-    await ctx.replyWithHTML(
-      "Please enter your Date of Birth\n\n<b>Format: DD/MM/YYYY</b> \n\n<b><i>Example: 01/01/2000</i></b>"
+
+    const msg = await ctx.replyWithHTML(
+      "Please enter your Date of Birth\n\n<b>Format: DD/MM/YYYY</b> \n\n<b><i>Example: 01/01/2000</i></b>",
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "🔙 Back",
+                callback_data: "back_to_2",
+              },
+            ],
+          ],
+        },
+      }
     );
+    ctx.scene.session.dobMsgId = msg.message_id;
     return ctx.wizard.next();
   },
+
+  // Wizard Step 4
   async (ctx) => {
-    let dob: string = (ctx.message as any)?.text;
+    if (
+      ctx.has(callbackQuery("data")) &&
+      ctx.callbackQuery.data === "back_to_2"
+    ) {
+      ctx.wizard.selectStep(2);
+
+      await deleteMessage(ctx, ctx.scene.session.dobMsgId);
+      if (typeof ctx.wizard.step === "function") {
+        return ctx.wizard.step(ctx, async () => {});
+      }
+    }
+    if (!ctx.has(message("text"))) {
+      return await ctx.reply("Please enter a valid date of birth");
+    }
+    let dob: string = ctx.message.text;
     if (!dob) {
       return await ctx.reply("Please enter a valid date of birth");
     }
@@ -103,6 +227,7 @@ const resultWizard = new Scenes.WizardScene<CustomContext>(
     } catch (error) {
       return await ctx.reply("Please enter a valid date of birth");
     }
+    await deleteMessage(ctx, ctx.scene.session.dobMsgId);
     ctx.scene.session.dob = dob;
     try {
       const waitingMsg = await ctx.reply("Fetching result.. Please wait..");
