33413ea9ecafc0b4f480ecf6b0774ee6e0d22a06
Devadathan M B <devadathanmb@gmail.com>
2024-03-05 22:26:37 +0530

    feat: update logging middleware

--- src/middlewares/loggingMiddleware.ts
+++ src/middlewares/loggingMiddleware.ts
@@ -6,14 +6,11 @@ import { CustomContext } from "@/types/customContext.type";
 const loggingMiddleware: Middleware<CustomContext> = async (ctx, next) => {
   const time = new Date().toString();
   console.log(
-    `➡️ [${time}] ${ctx.updateType} from ${
+    `-- [TIME : ${time}] [UPDATE : ${ctx.updateType}] [NAME : ${
       ctx.from?.first_name ? ctx.from.first_name : ""
-    } ${ctx.from?.last_name ? ctx.from.last_name : ""} ID : ${ctx.from?.id}`
+    } ${ctx.from?.last_name ? ctx.from.last_name : ""}] [ID : ${ctx.from?.id}]`
   );
-  console.log(ctx.message);
-  /* if (ctx.updateType === "message") console.log(ctx.message); */
-  /* else if (ctx.updateType === "callback_query") console.log(ctx.callbackQuery); */
+  console.log(ctx.update);
   return next();
 };
-
 export default loggingMiddleware;
