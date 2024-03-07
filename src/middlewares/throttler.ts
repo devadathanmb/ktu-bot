af698d222ebba8399cbb44dae8bee80256b450c3
Devadathan M B <devadathanmb@gmail.com>
2024-03-07 22:23:17 +0530

    refactor: disable inbound throttling

--- src/middlewares/throttler.ts
+++ src/middlewares/throttler.ts
@@ -1,9 +1,17 @@
 // Throttler middleware to avoid abuse by spam
 import { telegrafThrottler, ThrottlerOptions } from "telegraf-throttler";
 
-const opts: ThrottlerOptions = {
-  inKey: "chat",
-};
+let opts: ThrottlerOptions = {};
+
+/*
+  In PRODUCTION, that is in webhook mode, each update from chat, that is per user update is sent one by one.
+  Which means, telegram itself rate limits in on their side before sending the request. Hence we do not need to do it again here. 
+  NOTE: Currently, telegraf-throttler doesn't support disabling one kind of throttling. Hence we are just passing default Bottleneck values.
+  This needs to be fixed on throttler possibly with a PR.
+*/
+if (process.env.ENV_TYPE === "PRODUCTION") {
+  opts.in = {};
+}
 
 const throttler = telegrafThrottler(opts);
 
