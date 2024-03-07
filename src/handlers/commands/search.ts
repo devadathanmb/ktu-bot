c065023032af241ec2f00d1f1066b2d757acc0d4
Devadathan M B <devadathanmb@gmail.com>
2024-03-07 19:10:36 +0530

    feat: add a try now button for inline query search

--- src/handlers/commands/search.ts
+++ src/handlers/commands/search.ts
@@ -4,16 +4,25 @@ async function search(ctx: CustomContext) {
   const replyMsg = `
 Please use inline query to search for a KTU notification.
 
-Type <code>@ktu_results_bot query</code> in the bot chat and then choose the notification you want.
+Type <code>@ktu_results_bot query</code> in the bot chat and then choose the notification you want from the search results.
 
 <b>Examples:</b>
 
 • <code>@ktu_results_bot btech</code> : To search for <b>BTech</b> related notifications
 
 • <code>@ktu_results_bot exam registration</code> : To search for <b>exam registration</b> related notifications
-
-<i>Copy the above examples and paste it in the bot chat to see the results.</i>
 `;
-  await ctx.replyWithHTML(replyMsg);
+  await ctx.replyWithHTML(replyMsg, {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "🔍 Try now",
+            switch_inline_query_current_chat: "exam registration",
+          },
+        ],
+      ],
+    },
+  });
 }
 export default search;
