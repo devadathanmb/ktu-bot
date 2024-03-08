5ae24f2fddb9fb4d86a121cd9a9eb1a422f69c27
Devadathan M B <devadathanmb@gmail.com>
2024-03-08 16:53:49 +0530

    feat: add start personal chat button in non-private chat reply

--- src/handlers/commands/result.ts
+++ src/handlers/commands/result.ts
@@ -3,7 +3,18 @@ import { CustomContext } from "@/types/customContext.type";
 async function result(ctx: CustomContext) {
   const chatType = ctx.chat?.type;
   if (chatType !== "private") {
-    await ctx.reply("Result lookup is only available in private chat");
+    await ctx.reply("Result lookup is only available in private chat", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Start Private Chat 🚀",
+              url: `https://t.me/${ctx.botInfo?.username}?start`,
+            },
+          ],
+        ],
+      },
+    });
     return;
   }
   await ctx.scene.enter("result-wizard");
