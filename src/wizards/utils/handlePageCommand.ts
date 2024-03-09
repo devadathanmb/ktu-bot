810065d75933789491bcaaf6d6d0c58e312b1b56
Devadathan M B <devadathanmb@gmail.com>
2024-03-09 19:13:10 +0530

    feat: add utility function to handle quick page jump

---
+++ src/wizards/utils/handlePageCommand.ts
@@ -0,0 +1,21 @@
+import { CustomContext } from "@/types/customContext.type";
+import { message } from "telegraf/filters";
+
+async function handlePageCommand(
+  ctx: CustomContext,
+  deleteMsgFn: (ctx: CustomContext, msgId: number) => Promise<void>,
+  showItemsFn: (ctx: CustomContext) => Promise<void>
+) {
+  if (!ctx.has(message("text"))) {
+    return await ctx.reply("Please enter a valid page number.");
+  }
+  const page = Number.parseInt(ctx.message.text.split(" ")[1]);
+  if (isNaN(page) || page < 1) {
+    return await ctx.reply("Please enter a valid page number.");
+  }
+  ctx.scene.session.pageNumber = page - 1;
+  await deleteMsgFn(ctx, ctx.scene.session.tempMsgId);
+  await showItemsFn(ctx);
+}
+
+export default handlePageCommand;
