9d1025f79eb995323a1bbf1851cdd2ad25ceaa76
Devadathan M B <devadathanmb@gmail.com>
2024-03-19 18:21:19 +0530

    style: change summary message markup

--- src/utils/formatSummaryMessage.ts
+++ src/utils/formatSummaryMessage.ts
@@ -6,13 +6,13 @@ function formatSummaryMessage(summary: any) {
     : summary.firstName;
 
   return (
-    `<u><b>Result Details</b></u>\n\n\n` +
-    `<b>Name:</b> <b>${fullName}</b>\n\n` +
-    `<b>Branch:</b> <b>${summary.branch}</b>\n\n` +
-    `<b>Semester:</b> <b>${summary.semester}</b>\n\n` +
-    `<b>Register No:</b> <b>${summary.registrerNo}</b>\n\n` +
-    `<b>Institution:</b> <b>${summary.institutionName}</b>\n\n` +
-    `<b>Result Name:</b> <b>${summary.resultName}</b>\n\n`
+    `<u><b>Result Details</b></u>\n\n` +
+    `<b>• Name:</b> <b>${fullName}</b>\n\n` +
+    `<b>• Branch:</b> <b>${summary.branch}</b>\n\n` +
+    `<b>• Semester:</b> <b>${summary.semester}</b>\n\n` +
+    `<b>• Register No:</b> <b>${summary.registrerNo}</b>\n\n` +
+    `<b>• Institution:</b> <b>${summary.institutionName}</b>\n\n` +
+    `<b>• Result Name:</b> <b>${summary.resultName}</b>\n\n`
   );
 }
 
