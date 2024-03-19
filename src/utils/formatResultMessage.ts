f865201087168d63266fb5a8dbecbac1178a5d4a
Devadathan M B <devadathanmb@gmail.com>
2024-03-19 18:51:08 +0530

    fix: fix bug where all course results not properly appended

--- src/utils/formatResultMessage.ts
+++ src/utils/formatResultMessage.ts
@@ -1,13 +1,14 @@
 import { ResultDetails } from "@/types/types";
 import shortenCourse from "./shortenCourse";
 function formatResultMessage(resultDetails: ResultDetails[], sgpa: string) {
+  const SPLIT_LIMIT = 8;
   let messages: string[] = [];
   let message = "<u><b>Your Exam Results</b></u>\n\n";
 
   // If result payload contains lots of courses, then we need split the message into multiple messages
 
   for (let i = 0; i < resultDetails.length; i += 1) {
-    if (i % 6 === 0 && i !== 0) {
+    if (i % SPLIT_LIMIT === 0 && i !== 0) {
       messages.push(message);
       message = "";
     }
@@ -17,6 +18,12 @@ function formatResultMessage(resultDetails: ResultDetails[], sgpa: string) {
     message += `   - <b>Earned credits:</b> <b>${
       resultDetails[i].credits ? resultDetails[i].credits : 0
     }</b>\n\n\n`;
+  }
+
+  // If total pushed messages are less than the length of resultDetails, then push the remaining message
+  // then push the final remaining message
+  if (messages.length * SPLIT_LIMIT < resultDetails.length) {
+    messages.push(message);
   }
 
   messages[messages.length - 1] += `<b>SGPA:</b> <b>${sgpa}</b>\n\n`;
