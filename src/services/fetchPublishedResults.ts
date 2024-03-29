4f49ea531724bd1527933593c36c2be6131d0171
Devadathan M B <devadathanmb@gmail.com>
2024-03-30 00:08:47 +0530

    fix: build error due to missing param

--- src/services/fetchPublishedResults.ts
+++ src/services/fetchPublishedResults.ts
@@ -24,6 +24,7 @@ async function fetchPublishedResults(
       resultName: result.resultName,
       examDefId: result.examDefId,
       schemeId: result.schemeId,
+      publishDate: result.publishDate,
     }));
 
     // TODO: Remove this when the API is fixed
