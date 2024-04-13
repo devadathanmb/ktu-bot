e9195a2dbdedd24c61c7628511c89de5878a174f
Devadathan M B <devadathanmb@gmail.com>
2024-04-13 23:49:15 +0530

    modify server error message

--- src/errors/ServerError.ts
+++ src/errors/ServerError.ts
@@ -1,6 +1,6 @@
 class ServerError extends Error {
   constructor(
-    message: string = "KTU servers are having issues right now. Please try again later."
+    message: string = "KTU servers are having issues right now. Please try again later.\n\nUse /serverstatus to check KTU server status."
   ) {
     super(message);
     this.name = "ServerError";
