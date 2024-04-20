f3782cedd9035e956544b2c7a9ef523693a54363
Devadathan M B <devadathanmb@gmail.com>
2024-04-20 21:23:23 +0530

    feat: drop pending updates in webhook deploy as well

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,9 @@ const launchBot = async () => {
       webhook: {
         domain: process.env.WEBHOOK_DOMAIN!,
         port: Number(process.env.WEBHOOK_PORT),
-        // NOTE: This needs to be tested to see how much concurrent connections can the bot server handle
         maxConnections: 100,
       },
+      dropPendingUpdates: true,
     });
     if (bot)
       bot.telegram.getMe().then((res) => {
