0323827e0ddb29a8eb058d3face03d2058672600
Devadathan M B <devadathanmb@gmail.com>
2024-06-23 01:21:27 +0530

    update warning middleware

--- src/middlewares/warning.ts
+++ src/middlewares/warning.ts
@@ -3,7 +3,7 @@ import { CustomContext } from "types/customContext.type";
 
 const warningMiddleWare: Middleware<CustomContext> = async (ctx, next) => {
   if (ctx.message && ctx.text) {
-    if (ctx.text === "/result" || ctx.text === "/oldresults") {
+    if (ctx.text === "/oldresults") {
       await ctx.reply("Sorry, result checking is temporarily disabled.");
       return;
     }
